4642aa9acdd7e8ec7f33b944f17d18a7c58c7c50
Cilli177 <109823465+Cilli177@users.noreply.github.com>
2026-01-27 17:35:02 +0000

    ajuste

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -6,8 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// --- CONEXÃO COM A SUA API REAL NA RAILWAY ---
-// Usando o link que você confirmou que está online
+// --- CONEXÃO COM A API NA RAILWAY ---
 builder.Services.AddScoped(sp => new HttpClient 
 { 
     BaseAddress = new Uri("https://positive-reprieve-production-04d0.up.railway.app/") 
